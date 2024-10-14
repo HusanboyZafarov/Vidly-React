396244bccb7dc692f1652a52280ece1c8e539cf7
Husanboy Zafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2024-10-14 18:33:41 +0500

    Update MovieForm.jsx

--- src/components/MovieForm/MovieForm.jsx
+++ src/components/MovieForm/MovieForm.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { useParams, useNavigate } from 'react-router-dom';
 
-const MovieForm = ({ }) => {
+const MovieForm = () => {
     const { id } = useParams()
     const navigate = useNavigate()
     return (
         <div >
-            <h1>MovieForm - {useParams().id}</h1>
+            <h1>MovieForm - {id}</h1>
             <button className='btn btn-primary' onClick={() => navigate("/movies")}>Save</button>
         </div>
     );
